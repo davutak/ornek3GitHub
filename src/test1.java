c9c4904997e2bc5e0316f50e1047f03206f70605
Davut Akhoroz <dakhoroz@gmail.com>
2020-12-20 19:14:07 +0100

    2.islem

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,10 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("1.islem");
+        System.out.println("2.islem");
+    }
+
+    public static void yeniMetodSepe() {
+        System.out.println("Sepet Kismi Tamam");
     }
 }
